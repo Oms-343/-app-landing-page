d775ad3379586db873ead4cd7b80b108f3e64aba
Oms-343 <oms87473@gmail.com>
2024-08-28 14:22:18 +0530

    overlooked tailwind-config-bug

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,12 +2,16 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/sections/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+      },
+    },
   },
   plugins: [],
 };
