4fc6cb5aadd1a25dce969e6332087ef0a884880f
Oms-343 <oms87473@gmail.com>
2024-08-31 18:32:11 +0530

    adding client logo animations

--- src/sections/Clients.tsx
+++ src/sections/Clients.tsx
@@ -1,5 +1,7 @@
+"use client";
 import React from "react";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 import acmeLogo from "@/public/logo-acme.png";
 import quantamLogo from "@/public/logo-quantum.png";
@@ -13,7 +15,18 @@ export default function Clients() {
     <section className="py-8 md:py-12 bg-white">
       <div className="container">
         <div className="flex overflow-hidden [mask-image:linear-gradient(to_left,transparent,black,transparent)]">
-          <ul className="flex gap-14 flex-none">
+          <motion.ul
+            className="flex gap-14 flex-none pr-14"
+            animate={{
+              translateX: "-50%",
+            }}
+            transition={{
+              duration: 20,
+              repeat: Infinity,
+              ease: "linear",
+              repeatType: "loop",
+            }}
+          >
             <li>
               <Image src={acmeLogo} alt="acme-logo" className="client-image" />
             </li>
@@ -44,7 +57,39 @@ export default function Clients() {
             <li>
               <Image src={apexLogo} alt="apex-logo" className="client-image" />
             </li>
-          </ul>
+
+            {/*duplicating logos for that infinte animation effect */}
+            <li>
+              <Image src={acmeLogo} alt="acme-logo" className="client-image" />
+            </li>
+            <li>
+              <Image
+                src={quantamLogo}
+                alt="quantam-logo"
+                className="client-image"
+              />
+            </li>
+            <li>
+              <Image src={echoLogo} alt="echo-logo" className="client-image" />
+            </li>
+            <li>
+              <Image
+                src={celestialLogo}
+                alt="celestial-logo"
+                className="client-image"
+              />
+            </li>
+            <li>
+              <Image
+                src={pulseLogo}
+                alt="pulse-logo"
+                className="client-image"
+              />
+            </li>
+            <li>
+              <Image src={apexLogo} alt="apex-logo" className="client-image" />
+            </li>
+          </motion.ul>
         </div>
       </div>
     </section>
